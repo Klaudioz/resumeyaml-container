4b1affe1e2b726fee9607711d2802721c5109e17
Claudio Canales <ccanales@progrexion.com>
2019-05-02 19:42:28 -0600

    Fix issue with deployment file

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -10,9 +10,8 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
-find . -type f -name "resumeyaml.yml"
-envsubst <./kube/resumeyaml.yml >./kube/resumeyaml.yml.out
-mv ./kube/resumeyaml.yml.out ./kube/resumeyaml.yml
+envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out
+mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
