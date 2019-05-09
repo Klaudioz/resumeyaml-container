a469cb5be93f069353bfbb57d44202a922984914
Claudio Canales <ccanales@progrexion.com>
2019-05-08 21:31:10 -0600

    Add terraform part

--- scripts/ci-deploy-local.sh
+++ scripts/ci-deploy-local.sh
@@ -10,15 +10,13 @@ export COMMIT_SHA1=$COMMIT_SHA1
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
-envsubst <./kube/do-sample-deployment.yml >./kube/do-sample-deployment.yml.out
-mv ./kube/do-sample-deployment.yml.out ./kube/do-sample-deployment.yml
+envsubst <./do-sample-deployment.yml >./do-sample-deployment.yml.out
+mv ./do-sample-deployment.yml.out ./do-sample-deployment.yml
 
 echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
 
-./kubectl \
-  --kubeconfig=/dev/null \
-  --insecure-skip-tls-verify 
+kubectl \
   --server=$KUBERNETES_SERVER \
   --certificate-authority=cert.crt \
   --token=$KUBERNETES_TOKEN \
-  apply -f ./kube/
+  apply -f ../kube/
